ab7bed8adcb31a277b47c1bd111afbd8d9f9cb32
Harrison Milbradt <harrison.milbradt@gmail.com>
2019-05-25 10:08:13 -0700

    Sprucing the about

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -4,44 +4,97 @@ import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
 
-export const AboutPageTemplate = ({ title, content, contentComponent }) => {
+export const AboutPageTemplate = ({ title, image, content, contentComponent }) => {
   const PageContent = contentComponent || Content
 
   return (
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
-              </h2>
-              <PageContent className="content" content={content} />
-            </div>
+      <section className="section section--gradient">
+          <div className="container">
+              <div className="columns">
+                  <div className="column is-10 is-offset-1">
+                      <div className="section">
+                          <div
+                              className="full-width-image margin-top-0"
+                              style={{
+                                  backgroundImage: `url(${
+                                      !!image.childImageSharp
+                                          ? image.childImageSharp.fluid.src
+                                          : image
+                                  })`,
+                                  backgroundPosition: `top left`,
+                                  backgroundAttachment: `fixed`
+                              }}
+                          >
+                              <div
+                                  style={{
+                                      display: "flex",
+                                      height: "150px",
+                                      lineHeight: "1",
+                                      justifyContent: "space-around",
+                                      alignItems: "left",
+                                      flexDirection: "column"
+                                  }}
+                              >
+                                  <h1
+                                      className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
+                                      style={{
+                                          boxShadow:
+                                              "rgb(0, 0, 0) 0.5rem 0px 0px, rgb(0, 0, 0) -0.5rem 0px 0px",
+                                          backgroundColor: "rgb(0, 0, 0)",
+                                          color: "white",
+                                          lineHeight: "1",
+                                          padding: "0.25em"
+                                      }}
+                                  >
+                                      {title}
+                                  </h1>
+                                  <h3
+                                      className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
+                                      style={{
+                                          boxShadow:
+                                              "rgb(0, 0, 0) 0.5rem 0px 0px, rgb(0, 0, 0) -0.5rem 0px 0px",
+                                          backgroundColor: "rgb(0, 0, 0)",
+                                          color: "white",
+                                          lineHeight: "1",
+                                          padding: "0.25em"
+                                      }}
+                                  >
+                                      {subtitle}
+                                  </h3>
+                              </div>
+                          </div>
+                          <PageContent
+                              className="content"
+                              content={content}
+                          />
+                      </div>
+                  </div>
+              </div>
           </div>
-        </div>
-      </div>
-    </section>
+      </section>
   )
 }
 
 AboutPageTemplate.propTypes = {
-  title: PropTypes.string.isRequired,
-  content: PropTypes.string,
-  contentComponent: PropTypes.func,
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string,
+    image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+    contentComponent: PropTypes.func
 }
 
 const AboutPage = ({ data }) => {
   const { markdownRemark: post } = data
 
   return (
-    <Layout>
-      <AboutPageTemplate
-        contentComponent={HTMLContent}
-        title={post.frontmatter.title}
-        content={post.html}
-      />
-    </Layout>
+      <Layout>
+          <AboutPageTemplate
+              contentComponent={HTMLContent}
+              title={post.frontmatter.title}
+              subtitle={post.frontmatter.subtitle}
+              image={post.frontmatter.image}
+              content={post.html}
+          />
+      </Layout>
   )
 }
 
@@ -56,7 +109,14 @@ export const aboutPageQuery = graphql`
     markdownRemark(id: { eq: $id }) {
       html
       frontmatter {
-        title
+        title,
+        image {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
       }
     }
   }
